67f23b9997ed47fb3f9f3c7b92ac27b222a07e09
ManpreetVB <kaurkaursandhu@gmail.com>
2024-06-20 19:57:43 +1200

    first commit

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import loading1 from '../loading1.gif';
+const Spinner=()=>{
+  
+    return (
+      <div className='text-center'>
+        <img src ={loading1} alt="loading"/>
+      </div>
+    )
+  
+}
+
+export default Spinner
